d993220c1d70d1085f828364aad59c49e23a32f0
aditya <adityalawande17@gmail.com>
2026-02-25 00:37:51 +0530

    Inserted render link for backend

--- studentkart-frontend/src/services/api.js
+++ studentkart-frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000";
+//const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://studentkart.onrender.com";
 
 export const getMaterials = async (year, subject, category) => {
   let url = `${BASE_URL}/materials`;
